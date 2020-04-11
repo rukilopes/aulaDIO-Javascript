18c34fe9be728e99ffac378e50e390ffa2483354
Emily Piazza Lopes <37214001+rukilopes@users.noreply.github.com>
2020-04-11 01:14:36 -0300

    página inicial e alguns testes no main.js

---
+++ JS/main.js
@@ -0,0 +1,66 @@
+var data = new Date();
+console.log("data completa: " + data);
+console.log("mês: " + (data.getMonth()+1));
+console.log("minutos: " + data.getMinutes());
+console.log("dia: " + data.getDay());
+console.log("horas: " + data.getHours());
+
+
+
+// for(var count = 0 ; count <= 5 ; count ++){
+//     alert(count);
+// };
+
+
+// var count = 0;
+// while(count <= 5){
+//     console.log(count);
+//     alert(count);
+//     count++;
+// }
+
+
+// var idade = prompt("Qual sua idade?");//usuário digita a idade no alerta
+// if (idade >= 18){
+//     alert("maior de idade");
+// } else {
+//     alert("menor de idade");
+// }
+
+
+// var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+
+// var fruta = {nome:"maça", cor:"vermelha"}
+// console.log(fruta);
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+// var lista = ["maça","pêra","laranja"];
+// console.log(lista); //trata como array
+// console.log(lista.toString()); //trata como string
+// console.log(lista[0]);
+// console.log(lista.toString()[0]);//primeiro caractére da string x
+// console.log(lista.join(" - "));//separar itens por -
+// lista.pop();//exclui item da lista
+// lista.push("uva"); //adiciona item na lista
+// console.log(lista.length); //tamanho da lista
+// alert(lista[1]); //item específico da lista
+
+
+
+// var nome = "Emily Piazza Lopes";
+// var n1 = 5;
+// var n2 = 3;
+// var frase = "Olá, tudo bem?";
+
+
+// alert(nome + " tem " + idade + " anos");
+// alert(idade + idade2);
+
+// console.log(nome);
+// console.log(n1 * n2);
+// console.log(frase.replace("Olá","Bom dia"));
+// console.log(frase.toUpperCase());
